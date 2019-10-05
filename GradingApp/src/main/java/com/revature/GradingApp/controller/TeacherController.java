04e4ed087a3d875b0efc59e39042f33138f658ff
Priyanka-Sri <54095647+Priyanka-Sri@users.noreply.github.com>
2019-10-05 22:03:10 +0530

    Add files via upload

---
+++ GradingApp/src/main/java/com/revature/GradingApp/controller/TeacherController.java
@@ -0,0 +1,46 @@
+package com.revature.GradingApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.revature.grading.dao.RegisterDao;
+import com.revature.grading.model.Admindetails;
+@RestController
+public class TeacherController {
+	@GetMapping("Teacherlogin")
+	 public static String Tlogin(@RequestParam("name") String name, @RequestParam("password") String password) {
+	        
+	        String errorMessage = null;
+	        
+	        RegisterDao dao =new RegisterDao();
+	        Admindetails detail  = null;
+	        try {
+	        	detail=dao.tech1login(name,password);
+	        	
+	        } catch (Exception e) {
+	            //e.printStackTrace();
+	            errorMessage = e.getMessage();
+	        }       
+	         
+	        // Prepare JSON Object
+	        String json = null;
+	        Gson gson = new Gson();
+	        if( detail != null || errorMessage == null) {
+	             json = gson.toJson(detail);
+	        }
+	        else {
+	        	errorMessage = "Invalid Username and password";
+	            JsonObject obj = new JsonObject();
+	            obj.addProperty("errorMessage", errorMessage);
+	            json = obj.toString();
+	        }
+	         
+	         
+	        return json;
+	         
+	    }
+
+}
